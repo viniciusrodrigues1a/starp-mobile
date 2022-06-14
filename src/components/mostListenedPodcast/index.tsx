83677d447c79378587e490ad001be1e275d5500b
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2022-06-13 22:08:19 -0300

    Styling the most listened section
    Co-authored-by: Wellington Rufino Jr. <wellingtonjr53@outlook.com.br>

---
+++ src/components/mostListenedPodcast/index.tsx
@@ -0,0 +1,120 @@
+import React from "react";
+import { View, Text, Image, TouchableOpacity, StyleSheet } from "react-native";
+
+// @ts-ignore
+import Star from "../../../assets/star.png";
+// @ts-ignore
+import Headphones from "../../../assets/headphones.png";
+// @ts-ignore
+import Clock from "../../../assets/clock.png";
+// @ts-ignore
+import MoreVertical from "../../../assets/more-vertical.png";
+
+type MostListenedPodcastProps = {
+  title: string;
+  artist: string;
+  length: string;
+  timesListened: string;
+  timesStarred: string;
+  image?: string;
+};
+
+export function MostListenedPodcast({
+  artist,
+  length,
+  timesListened,
+  timesStarred,
+  title,
+  image = "https://i.ytimg.com/vi/kU4ZEyWVwn8/maxresdefault.jpg",
+}: MostListenedPodcastProps) {
+  return (
+    <TouchableOpacity style={styles.container}>
+      <View style={styles.topContent}>
+        <Image
+          style={{
+            width: "30%",
+            height: "auto",
+            marginRight: 16,
+            borderRadius: 10,
+          }}
+          source={{
+            uri: "https://i.ytimg.com/vi/kU4ZEyWVwn8/maxresdefault.jpg",
+          }}
+        />
+        <View>
+          <Text style={styles.titleText}>{title}</Text>
+          <Text style={styles.artistText}>{artist}</Text>
+        </View>
+      </View>
+      <View style={styles.footerContent}>
+        <View style={styles.relatedInformationsContent}>
+          <View style={styles.relatedInformationsItem}>
+            <Image source={Clock} />
+            <Text style={styles.footerText}>{length}</Text>
+          </View>
+          <View style={styles.relatedInformationsItem}>
+            <Image source={Headphones} />
+            <Text style={styles.footerText}>{timesListened}</Text>
+          </View>
+          <View style={styles.relatedInformationsItem}>
+            <Image source={Star} />
+            <Text style={styles.footerText}>{timesStarred}</Text>
+          </View>
+        </View>
+        <TouchableOpacity>
+          <Image source={MoreVertical} />
+        </TouchableOpacity>
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#141414",
+    borderColor: "#333333",
+    borderWidth: 1,
+    padding: 24,
+    borderRadius: 10,
+  },
+  topContent: {
+    flexDirection: "row",
+    alignContent: "center",
+    justifyContent: "space-between",
+    marginBottom: 16,
+  },
+  titleText: {
+    marginBottom: 4,
+    fontFamily: "spaceGrotesk400",
+    fontSize: 23,
+    color: "#FFF",
+    flexShrink: 1,
+    width: "80%",
+  },
+  artistText: {
+    marginBottom: 4,
+    fontFamily: "spaceGrotesk500",
+    fontSize: 12,
+    color: "#767676",
+  },
+  footerContent: {
+    flexDirection: "row",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  footerText: {
+    color: "#767676",
+    fontFamily: "spaceGrotesk500",
+    fontSize: 12,
+    marginLeft: 8,
+  },
+  relatedInformationsContent: {
+    flexDirection: "row",
+  },
+  relatedInformationsItem: {
+    flexDirection: "row",
+    marginRight: 16,
+  },
+});
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,28 +1,87 @@
 import React from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  ScrollView,
+  FlatList,
+  StyleSheet,
+} from "react-native";
 
 // @ts-ignore
 import Logo from "../../../assets/logo.png";
 // @ts-ignore
 import Bell from "../../../assets/bell.png";
+// @ts-ignore
+import ArrowRight from "../../../assets/arrow-right.png";
+
+import { MostListenedPodcast } from "../../components/mostListenedPodcast";
 
 export function Home() {
   return (
-    <View style={styles.headerView}>
-      <View style={styles.headerViewFlex}>
-        <Image source={Logo} />
-        <Image source={Bell} />
+    <ScrollView style={styles.container}>
+      <View style={styles.headerView}>
+        <View style={styles.headerViewFlex}>
+          <Image source={Logo} />
+          <Image source={Bell} />
+        </View>
+
+        <Text style={styles.headerViewTitle}>
+          Olá, <Text style={styles.headerViewTitleSpan}>Jonas</Text>
+        </Text>
+        <Text style={styles.headerViewSubtitle}>
+          O que deseja escutar hoje?
+        </Text>
       </View>
 
-      <Text style={styles.headerViewTitle}>
-        Olá, <Text style={styles.headerViewTitleSpan}>Jonas</Text>
-      </Text>
-      <Text style={styles.headerViewSubtitle}>O que deseja escutar hoje?</Text>
-    </View>
+      <View style={styles.mostListenedView}>
+        <View style={styles.mostListenedViewFlex}>
+          <Text style={styles.mostListenedViewTitle}>Em alta</Text>
+          <View style={styles.seeMoreView}>
+            <Text style={styles.seeMoreText}>ver todos</Text>
+            <Image source={ArrowRight} />
+          </View>
+        </View>
+
+        <FlatList
+          horizontal={true}
+          data={[
+            {
+              id: "0",
+              title: "LUCAS INUTILISMO - Podpah #404",
+              artist: "Flow Podcast",
+              length: "1:24h",
+              timesListened: "407K",
+              timesStarred: "65K",
+            },
+            {
+              id: "1",
+              title: "LUCAS INUTILISMO - Podpah #404",
+              artist: "Flow Podcast",
+              length: "1:24h",
+              timesListened: "407K",
+              timesStarred: "65K",
+            },
+          ]}
+          renderItem={({ item }) => (
+            <MostListenedPodcast
+              artist={item.artist}
+              length={item.length}
+              timesListened={item.timesListened}
+              timesStarred={item.timesStarred}
+              title={item.title}
+            />
+          )}
+          ItemSeparatorComponent={() => <View style={styles.separator}></View>}
+          keyExtractor={(item) => item.id}
+        />
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: "#141414" },
   headerView: {
     paddingHorizontal: 24,
     paddingVertical: 48,
@@ -49,5 +108,33 @@ const styles = StyleSheet.create({
     color: "#767676",
     fontFamily: "spaceGrotesk500",
     fontSize: 18,
+  },
+  mostListenedView: {
+    paddingLeft: 24,
+    paddingVertical: 48,
+  },
+  mostListenedViewFlex: {
+    marginBottom: 24,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  mostListenedViewTitle: {
+    color: "#FFFFFF",
+    fontFamily: "spaceGrotesk500",
+    fontSize: 20,
+  },
+  seeMoreView: {
+    paddingRight: 24,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  seeMoreText: {
+    color: "#767676",
+    fontFamily: "spaceGrotesk500",
+    fontSize: 16,
+  },
+  separator: {
+    width: 24,
   },
 });
