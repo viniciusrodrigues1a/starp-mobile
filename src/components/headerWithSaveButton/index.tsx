043b00a4a23272cf8d47a618ae64e28ba12deec3
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2022-08-20 17:49:59 -0300

    headerWithSaveButton arrowLeft button now goes back

--- src/components/headerWithSaveButton/index.tsx
+++ src/components/headerWithSaveButton/index.tsx
@@ -4,15 +4,25 @@ import { Image, Text, TouchableOpacity, View } from "react-native";
 import { styles } from "./styles";
 
 import ArrowLeft from "../../assets/arrow-left.png";
+import { useNavigation } from "@react-navigation/native";
 
 type HeaderProps = {
   onSavePressed: () => void;
 };
 
 export function HeaderWithSaveButton({ onSavePressed }: HeaderProps) {
+  const { goBack } = useNavigation();
+
   return (
     <View style={styles.containerView}>
-      <Image source={ArrowLeft} style={styles.image} />
+      <TouchableOpacity
+        activeOpacity={1}
+        onPress={goBack}
+        hitSlop={{ left: 16, bottom: 16, right: 16, top: 16 }}
+      >
+        <Image source={ArrowLeft} style={styles.image} />
+      </TouchableOpacity>
+
       <TouchableOpacity onPress={onSavePressed}>
         <Text style={styles.saveButtonText}>Salvar</Text>
       </TouchableOpacity>
